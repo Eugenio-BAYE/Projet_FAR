a8ba354b9d7d823d284d4e33a17f1961eae63234
Eugénio <baye.eugenio.egnb@gmail.com>
2024-04-20 15:02:19 +0200

    fix : Cleaning code

--- server.c
+++ server.c
@@ -13,93 +13,107 @@
 #define globalMessageLenght 256
 #define MAX_CLIENT 10
 
+// ------------------------------------------------------
 int clients[MAX_CLIENT]; // Array to store client socket descriptors
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; // Mutex for thread-safe access to clients array
-int nbr_of_clients;
+int nbr_of_clients = 0;
 
-struct ThreadArgs {
-    int dSC_sender;
-    int dSC_receiver;
+struct handle_client_args {
+  int dSC_sender;
 };
-
+ /* broadcast_message : Send a message to every client except the one in parameter
+  * Precondition : Sender must be in the global clients listening, mutex must be free in order to access the global client list 
+  * Parameters : int sender(file descriptor of the sender), char* message(message to broadcast to the list), int message_size (size of the message)
+  * Returns : Nothing
+  */
 void broadcast_message(int sender, char *message, int message_size) {
-  printf("Message broadcasting\n");
-    pthread_mutex_lock(&mutex);
-    for (int i = 0; i < MAX_CLIENT; i++) {
-        if (clients[i] != 0 && clients[i] != sender) {
-            send(clients[i], message, message_size, 0);
-        }
+  printf("Message broadcasting : %s\n", message);
+  pthread_mutex_lock(&mutex);
+  for (int i = 0; i < MAX_CLIENT; i++) {
+    if (clients[i] != 0 && clients[i] != sender) {
+      send(clients[i], message, message_size, 0);
     }
-    pthread_mutex_unlock(&mutex);
+  }
+  pthread_mutex_unlock(&mutex);
 }
-
+/* handle_client : Thread-dedicated function to handle each client
+ * Precondition : No more than <MAX_CLIENT> clients to handle
+ * Parameters : struct handle_client_args -> int dSC_sender (file descriptor of the sender client)
+ * Returns : Nothing
+ */ 
 void* handle_client(void* args) {
-  struct ThreadArgs* t_args = (struct ThreadArgs*)args;
+  struct handle_client_args* t_args = (struct handle_client_args*)args;
   int dSC_sender = t_args->dSC_sender;
-  int dSC_receiver = t_args->dSC_receiver;
-  int isRunning = 1;
-  size_t size = 0;
 
-  while (isRunning) {
-    printf("Start listening\n");
-    if(0){
-      printf("Wrong size\n");
+  while (1) {
+    char msg[globalMessageLenght];
+
+    if(receive_message(dSC_sender, msg, globalMessageLenght) <= 0) {
+      pthread_mutex_lock(&mutex);
+      for (int i = 0; i < MAX_CLIENT; i++) {
+        if (clients[i] == dSC_sender) {
+          clients[i] = 0;
+          break;
+        }
+      }
+      nbr_of_clients--;
+      pthread_mutex_unlock(&mutex);
+      close(dSC_sender);
+      printf("Client disconnected\n");
+      pthread_exit(NULL);
       break;
     }
-    printf("Sized received : %zu\n", size);
-    size = 256;
-    if (size >= 0 && size < 1024){
-      printf("Allocating next msg\n");
-    
-    char msg[256];
-    if(receive_message(dSC_sender, msg, size) <= 0) {
-      isRunning = 0;
-      break;
-    }
-    broadcast_message(dSC_sender, msg, size);
-    printf("Message sent\n");
-    }
+    broadcast_message(dSC_sender, msg, globalMessageLenght);
   }
 }
 
 int main(int argc, char *argv[]) {
-
-  // Global to local variable to enhance performance
-  int msgLenght = globalMessageLenght; 
+  if(argc != 2){
+    printf("Usage : %s <port>\n", argv[0]);
+    perror("Can't run program");
+    exit(EXIT_FAILURE);
+  }
 
   printf("Starting program\n");
 
+  for (int i = 0; i < MAX_CLIENT; i++) {
+    clients[i] = 0;
+  }
+
   // Server socket connection
   int dS = new_server_socket(atoi(argv[1]));
-  printf("Listening mode\n");
-  
+  printf("Waiting for clients\n");
 
   while (1){
     int dSC = new_client_connection(dS);
+    while(nbr_of_clients>9){
+      perror("Too much clients to handle");
+      exit(EXIT_FAILURE);
+    }
+    
+    pthread_mutex_lock(&mutex);
+    nbr_of_clients++;
+    pthread_mutex_unlock(&mutex);
     pthread_t thread;
-
     // Add client to the clients array
     pthread_mutex_lock(&mutex);
     for (int i = 0; i < MAX_CLIENT; i++) {
-        if (clients[i] == 0) {
-            clients[i] = dSC;
-            break;
-        }
+      if (clients[i] == 0) {
+        clients[i] = dSC;
+        break;
+      }
     }
     pthread_mutex_unlock(&mutex);
-    
-    struct ThreadArgs arg = {dSC, dSC};
+
+    struct handle_client_args arg = {dSC};
 
     if (pthread_create(&thread, NULL, handle_client, (void*)&arg) != 0) {
       perror("pthread_create");
-    return 1;
+      return 1;
+    }
   }
-    printf("New thread created\n");
-  }
-  
-  
-  printf("Shutting down programm\n");
 
-  shutdown(dS, 2) ;
+  printf("Shutting down programm\n");
+  close(dS) ;
   printf("Bye bye\n");
 }
--- src/server_src/client_handling.c
+++ src/server_src/client_handling.c
@@ -11,14 +11,14 @@
  */
 int new_client_connection(int server_socket) {
 
-    struct sockaddr_in client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
+  struct sockaddr_in client_addr;
+  socklen_t client_addr_len = sizeof(client_addr);
 
-    // Client connection :
-    int client_socket = accept(server_socket, (struct sockaddr*) &client_addr, &client_addr_len);
-    printf("New client connected\n");
+  // Client connection :
+  int client_socket = accept(server_socket, (struct sockaddr*) &client_addr, &client_addr_len);
+  printf("New client connected\n");
 
-    return client_socket;
+  return client_socket;
 }
 
 /* receive_from_client : Get a message from a client in msg
@@ -29,9 +29,6 @@ int receive_message(int dSC, char msg[], int msgLenght){
   // Empty msg of all content
   memset(msg, '\0', msgLenght);
   int received_size = recv(dSC, msg, msgLenght, 0);
-  if (received_size>0){
-    printf("Message received from client : %s\n", msg);
-  }
   return received_size;
 }
 
@@ -41,17 +38,17 @@ int receive_message(int dSC, char msg[], int msgLenght){
  */
 
 int receive_msg_size(int dSC, size_t *size) {
-    size_t message_size;
-    printf("Create message size\n");
-    int received_size = recv(dSC, &message_size, sizeof(message_size), 0);
-    if (received_size > 0) {
-        printf("Message size received: %d\n", (int)message_size);
-        *size = message_size;
-    } else if (received_size == 0) {
-        printf("Client disconnected.\n");
-    } else {
-        perror("recv");
-    }
-    return received_size;
+  size_t message_size;
+  printf("Create message size\n");
+  int received_size = recv(dSC, &message_size, sizeof(message_size), 0);
+  if (received_size > 0) {
+    printf("Message size received: %d\n", (int)message_size);
+    *size = message_size;
+  } else if (received_size == 0) {
+    printf("Client disconnected.\n");
+  } else {
+    perror("recv");
+  }
+  return received_size;
 }
 

99d8cc7373215e4c42c4a203bf1b8dac00c06ce1
Eugénio <baye.eugenio.egnb@gmail.com>
2024-05-01 17:33:45 +0200

    feat : Add username to clients 	-Server asks client for username 	-Broadcast now displays username

--- src/server_src/client_handling.h
+++ src/server_src/client_handling.h
@@ -11,6 +11,18 @@ void free_client_list();
 void add_new_client(int dSC);
 int can_accept_new_client();
 void remove_client(int dSC);
+/* is_username_valid : Checks if username can be used
+ * Parameters : char username[] 
+ * Returns :  1 if it can be taken (SUCESS)
+ *            2 if it is too long (FAILURE)
+ *            3 if it is aldready taken (FAILURE)
+ */
+int is_username_valid(char username[]);
+void ask_username(int dSC);
+void update_username(int dSC, char username[]);
+
+int formated_msg_size(int dSC, int msg_size);
+void format_msg(char msg[],int dSC,int size, char formated_msg[]);
 
 /* broadcast_message : Send a message to every client except the one in parameter
   * Precondition : Sender must be in the global clients listening, mutex must be free in order to access the global client list 
